e55d6faa5acd17aa066100ff0e94a715353678cc
Marcelo Garcia <marcelof.garcia@yahoo.com.br>
2022-02-15 14:32:52 -0300

    update_15022022

--- aulas_modulo4.py
+++ aulas_modulo4.py
@@ -1,46 +1,35 @@
 
-
+#função ano bissexto
 def is_year_leap(year):
-    if year % 4 == 0:
-        if year % 100 != 0:
-            return True
-        elif year % 100 == 0:
-            if year % 400 != 0:
-                return False
-            elif year % 400 == 0:
-                return True
+    if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+        return True
     else:
         return False
 
-
-def days_in_month(year, month):
-    retorno = is_year_leap(year)
-    if retorno:
-        if month == 2:
-            days = 29
-        elif month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
-            days = 31
-        elif month == 4 or month == 6 or month == 9 or month == 11:
-            days = 30
+#função dias no mês
+def days_in_month(yr, mo):
+    bissexto = is_year_leap(yr)
+    if bissexto and mo == 2:
+        days = 29
     else:
-        if month == 2:
+        if mo == 2:
             days = 28
-        elif month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
+        elif mo == 1 or mo == 3 or mo == 5 or mo == 7 or mo == 8 or mo == 10 or mo == 12:
             days = 31
-        elif month == 4 or month == 6 or month == 9 or month == 11:
+        elif mo == 4 or mo == 6 or mo == 9 or mo == 11:
             days = 30
     return days
 
 
 test_years = [1900, 2000, 2016, 1987]
-test_months = [1, 3, 7, 11]
-test_results = [31, 31, 31, 30]
+test_months = [2, 2, 1, 11]
+test_results = [28, 29, 31, 30]
 for i in range(len(test_years)):
     yr = test_years[i]
     mo = test_months[i]
     print(yr, mo, "--> ", end="")
     results = days_in_month(yr, mo)
-    if results == test_results:
+    if results == test_results[i]:
         print("OK")
     else:
         print("Failed")
