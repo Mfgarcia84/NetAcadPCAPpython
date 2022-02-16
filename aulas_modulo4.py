d3a0b01681886e125a2a0128aa7849e7a7c1cac8
Marcelo Garcia <marcelof.garcia@yahoo.com.br>
2022-02-15 22:28:07 -0300

    update_15022022

--- aulas_modulo4.py
+++ aulas_modulo4.py
@@ -1,39 +1,15 @@
-
-#função ano bissexto
-def is_year_leap(year):
-    if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
-        return True
-    else:
-        return False
-
-#função dias no mês
-def days_in_month(yr, mo):
-    bissexto = is_year_leap(yr)
-    if bissexto and mo == 2:
-        days = 29
-    else:
-        if mo == 2:
-            days = 28
-        elif mo == 1 or mo == 3 or mo == 5 or mo == 7 or mo == 8 or mo == 10 or mo == 12:
-            days = 31
-        elif mo == 4 or mo == 6 or mo == 9 or mo == 11:
-            days = 30
-    return days
+def is_prime(num):
+    for n in range(2, num):
+        if num % n == 0:
+            return False
 
 
-test_years = [1900, 2000, 2016, 1987]
-test_months = [2, 2, 1, 11]
-test_results = [28, 29, 31, 30]
-for i in range(len(test_years)):
-    yr = test_years[i]
-    mo = test_months[i]
-    print(yr, mo, "--> ", end="")
-    results = days_in_month(yr, mo)
-    if results == test_results[i]:
-        print("OK")
-    else:
-        print("Failed")
 
+
+for i in range(1, 20):
+    if is_prime(i + 1):
+        print(i + 1, end=" ")
+print()
 
 
 
--- testes.py
+++ testes.py
@@ -1,13 +1,15 @@
+def is_a_triangle(a, b, c):
+    return a + b > c and b + c > a and c + a > b
 
-#Condição 1: Ser múltiplo de 4 e não ser múltiplo de 100
-#Condição 2: Ser múltiplo de 400 (se for múltiplo de 400 automaticamente é de 4)
 
-def anoBissexto(ano):
-    if (ano % 4 == 0 and ano % 100 != 0) or ano % 400 == 0:
-        return True
-    else:
+def is_a_right_triangle(a, b, c):
+    if not is_a_triangle(a, b, c):
         return False
+    if c > a and c > b:
+        return c ** 2 == a ** 2 + b ** 2
+    if a > b and a > c:
+        return a ** 2 == b ** 2 + c ** 2
 
 
-resultado = anoBissexto(1987)
-print(resultado)
+print(is_a_right_triangle(5, 3, 4))
+print(is_a_right_triangle(1, 3, 4))
